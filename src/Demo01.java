52a9bb4f61099157e7a046e39a0a7f9473acf99e
linda <linda@163.com>
2023-08-28 09:13:03 +0800

    第一次提交，添加了一个Demo01.java文件，完成helloworld的打印输出功能

---
+++ src/Demo01.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName Demo01
+ * @Description TODO
+ * @Author li.liu
+ * @Date 2023/8/28 09:03
+ * @Version 1.0
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("Demo01....");
+    }
+}
